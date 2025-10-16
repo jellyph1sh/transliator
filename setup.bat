dbf5034443c7a8f77cc06bf11915a27de6fe0111
evan <evan.ferron53@gmail.com>
2025-10-16 11:34:08 +0200

    add setup bat script

---
+++ setup.bat
@@ -0,0 +1,25 @@
+@echo off
+
+python -m venv .venv
+
+call .venv\Scripts\activate.bat
+
+REM install dependencies
+
+echo Verification et installation des dependances...
+
+pip show streamlit >nul 2>&1 || pip install streamlit
+
+pip show openai-whisper >nul 2>&1 || pip install openai-whisper
+
+pip show transformers >nul 2>&1 || pip install transformers
+
+pip show sentencepiece >nul 2>&1 || pip install sentencepiece
+
+pip show torch >nul 2>&1 || pip install torch
+
+pip show sacremoses >nul 2>&1 || pip install sacremoses
+
+echo Toutes les dependances sont installees!
+
+python -m streamlit run App.py
