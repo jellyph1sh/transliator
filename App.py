7f7e60657c7c80e81e23a9637be56da1763e7ccb
JellyPh1sh <sacha.sorgiati@gmail.com>
2025-10-16 11:43:49 +0200

    :building_construction: | Clean up the project structure

--- App.py
+++ App.py
@@ -1,6 +1,12 @@
 import streamlit as st
-import segment_to_subtitles as sts
-from components.video_uploader import video_uploader
 
-st.title('TranslIAtor')
-video_uploader(st)
+def main():
+    st.title('TranslIAtor')
+    uploaded_file = st.file_uploader(label="Choose your video", type="mp4", accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible", width="stretch")
+
+    if uploaded_file is not None:
+        with st.spinner("We prepare the video...", show_time=True):
+            # Déclencher transformation en format audio
+            st.video(uploaded_file, format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False, width="stretch")
+
+main()
--- components/video_player.py
+++
@@ -1,2 +0,0 @@
-def video_player(st, data):
-    st.video(data, format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False, width="stretch")
